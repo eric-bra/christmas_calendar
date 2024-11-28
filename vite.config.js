1088aba88848dd14d7c869b5d7df196e7252fbb8
Eric Brake <83550924+eric-bra@users.noreply.github.com>
2024-11-28 14:14:01 +0100

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/christmas_calendar(",
+  base: "/christmas_calendar/",
   plugins: [
     vue(),
     vueDevTools(),
